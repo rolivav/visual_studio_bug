9ca420eff936a5339cc30367dd797b5edaa7e5cb
rodrigo.oliva <rolivav@users.noreply.github.com>
2024-11-14 16:04:40 +0100

    Add some code.

---
+++ source/cmake_test.cpp
@@ -0,0 +1,28 @@
+﻿// cmake_test.cpp : Defines the entry point for the application.
+//
+
+#include "cmake_test.h"
+
+class Test {
+public:
+	bool worksFine() {
+		auto lamdba = [this]() {
+			auto otherLambda = [this]() {
+				return returnsTrue();
+			};
+			return otherLambda();
+		};
+		return lamdba();
+	}
+private:
+	bool returnsTrue() { return true; }
+};
+
+int main()
+{
+	Test test;
+	if (test.worksFine()) {
+		std::cout << "Works fine." << std::endl;
+	}
+	return 0;
+}
